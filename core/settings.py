01b7547f0993a11792e7194caab53ca9317199e1
aldotele <aldo.telese@hotmail.it>
2021-05-21 17:01:15 +0200

    whitenoise settings deleted

--- core/settings.py
+++ core/settings.py
@@ -129,18 +129,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-#MEDIA_URL = '/mediafiles/'
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles/mediafiles')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "staticfiles"),
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
 
