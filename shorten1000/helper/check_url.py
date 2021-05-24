0bcdcca5baa30d8e3e38462768f894f4cdb360ba
aldotele <aldo.telese@hotmail.it>
2021-05-24 11:35:57 +0200

    check if website exists, refactoring with external functions

--- shorten1000/helper/check_url.py
+++ shorten1000/helper/check_url.py
@@ -1,4 +1,20 @@
+import requests
+from requests.exceptions import ConnectionError
+
+
 def edit_url(url):
     if 'https://' not in url and 'http://' not in url:
         url = 'https://' + url
     return url
+
+
+def is_url_existing(url):
+    try:
+        request = requests.get(url)
+    except ConnectionError:
+        #print('Web site does not exist')
+        return False
+    else:
+        #print('Web site exists')
+        return True
+
