e40502b405ab6b5fdda77619dc50ae139091a0b8
aldotele <aldo.telese@hotmail.it>
2021-05-26 09:08:39 +0200

    conversion to Django forms with redirecting to another page

--- shorten1000/urls.py
+++ shorten1000/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from . import views
+from shorten1000.views import UrlCreateView, ShowShortUrlView, RedirectUrlView
+
 
 urlpatterns = [
     #path('', views.base, name='base'),
-    path('', views.index, name='test'),
-    path('create', views.create, name='create'),
-    path('<str:pk>', views.go, name='go')
+    path('', views.index, name='base'),
+    #path('create', views.create, name='create'),
+    #path('<str:pk>', views.go, name='go'),
+    # added
+    path('form', UrlCreateView.as_view(), name='form'),
+    #path('show', show, name='show'),
+    path('show/<int:pk>', ShowShortUrlView.as_view(), name='show_short_url'),
+    path('go/<slug:uuid>', RedirectUrlView.as_view(), name='redirect')
 ]
--- shorten1000/views.py
+++ shorten1000/views.py
@@ -7,10 +7,49 @@ from django.http import HttpResponse, HttpResponseNotAllowed, HttpResponseNotFou
 from django.core.exceptions import ValidationError, ObjectDoesNotExist
 from shorten1000.helper.check_url import edit_url, is_url_existing
 from shorten1000.helper.shorten_url import shorten
+# added below
+from django.views.generic import FormView, CreateView, DetailView
+from shorten1000.forms import UrlForm
+from django.urls import reverse_lazy, reverse
 
 
 def index(request):
-    return render(request, "index.html")
+    return render(request, "testempty.html")
+
+
+class UrlCreateView(CreateView):
+    model = Url
+    fields = ['link']
+    template_name = 'testform.html'
+    #success_url = reverse_lazy('show')
+
+    def get_success_url(self):
+        return reverse('show_short_url', args=[self.object.pk])
+
+
+class ShowShortUrlView(DetailView):
+    model = Url
+    context_object_name = 'url'
+    template_name = 'testshow.html'
+
+
+class RedirectUrlView(DetailView):
+    model = Url
+    slug_field = 'uuid'
+    slug_url_kwarg = 'uuid'
+
+    def render_to_response(self, context, **response_kwargs):
+        return redirect(self.object.link)
+
+
+# class UrlFormView(FormView):
+#     form_class = UrlForm
+#     template_name = 'form.html'
+
+
+# def show(request):
+#     context = {"prova": "... prova ..."}
+#     return render(request, "testshow.html", context)
 
 
 def create(request):
