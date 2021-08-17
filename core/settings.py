eda5484beeffbd532c82cf58aaca5cc48d7c00eb
aldotele <aldo.telese@hotmail.it>
2021-08-17 19:38:38 +0200

    secret key and debug production

--- core/settings.py
+++ core/settings.py
@@ -11,8 +11,12 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 from pathlib import Path
 import os
+from environs import Env
 
 import django_heroku
+
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p*_&-4hkx)^rd+l+o=9zixp=xa0v5a46ohyws6&(9s4%zcovab'
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = env.bool("DEBUG", default=True)
 
 ALLOWED_HOSTS = ['*']
 
